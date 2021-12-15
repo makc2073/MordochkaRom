f1085cefc1f7c7f04f5e89a7d402d206262d8d2d
макс ромаш <ro-mash@mail.ru>
2021-12-15 11:53:45 +0700

    2

--- MordochkaRom/Form1.cs
+++ MordochkaRom/Form1.cs
@@ -27,23 +27,116 @@ namespace MordochkaRom
         {
 
         }
-
-        private void FormClients_Load(object sender, EventArgs e)
+        string countlabel2;
+        string countlabel1;
+        string connectionString = "Data Source=10.10.1.24;Initial Catalog=UP_Romash;User ID=pro-41;Password=Pro_41student";
+        void View(string sql) // Метод вывода запроса в таблицу клиента
         {
-            string connectionString = "Data Source=10.10.1.24;Initial Catalog=UP_Romash;User ID=pro-41;Password=Pro_41student";
-            string sql = "SELECT * FROM Client";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                // Создаем объект DataAdapter
                 SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
-                // Создаем объект Dataset
                 DataSet ds = new DataSet();
-                // Заполняем Dataset
                 adapter.Fill(ds);
-                // Отображаем данные
-                dataGridView1.DataSource = ds.Tables[0];
+                dataGridViewClients.DataSource = ds.Tables[0];
+                connection.Close();
             }
+
+            string sqlExpression = "SELECT COUNT(*) FROM Client";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                int count = (int)command.ExecuteScalar();
+                countlabel2 = count.ToString();
+                connection.Close();
+            }
+        }
+
+        private void FormClients_Load(object sender, EventArgs e)
+        {        
+          View("SELECT * FROM Client");
+        }
+        
+        int str = 0;
+        string list;
+        int viewtop;
+        string toplist;
+        int top = 10;
+        int rows;
+        
+        void sheets(int countstr, int str)
+        {
+            viewtop = countstr * str;
+            toplist = Convert.ToString(viewtop);
+            list = Convert.ToString(countstr);
+            View("SELECT * FROM Client ORDER BY ID OFFSET " + toplist + " ROWS FETCH NEXT " + list + " ROWS ONLY;");
+            label1.Text = countlabel1 + "/" + countlabel2;
+        }
+        private void top10_Click(object sender, EventArgs e)
+        {
+            top = 10;
+            str = 0;
+            sheets(top, str);
+            rows = dataGridViewClients.Rows.Count - 1;           
+            countlabel1 = rows.ToString();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (str > 0)
+            {
+                str = str - 1;
+                sheets(top, str);
+            }
+            else
+            {
+                str = 0;
+                btnBack.Enabled = false;
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            str = str + 1;            
+            sheets(top, str);
+            btnBack.Enabled = true;
+            rows = rows + dataGridViewClients.Rows.Count - 1;
+            
+            countlabel1 = rows.ToString();
+
+        }
+
+        private void top50_Click(object sender, EventArgs e)
+        {
+            top = 50;
+            str = 0;
+            sheets(top, str);
+            rows = dataGridViewClients.Rows.Count - 1;
+            
+            countlabel1 = rows.ToString();
+        }
+
+        private void top100_Click(object sender, EventArgs e)
+        {
+            top = 100;
+            str = 0;
+            sheets(top, str);
+            rows = dataGridViewClients.Rows.Count - 1;
+           
+            countlabel1 = rows.ToString();
+        }
+
+        private void topAll_Click(object sender, EventArgs e)
+        {
+            View("SELECT * FROM Client");
+            rows = dataGridViewClients.Rows.Count - 1;
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
 
         }
     }
